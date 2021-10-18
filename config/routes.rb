2c236442705adbcda13b07660b53b2beef35fa3a
Michael Abbott <michael@mjabbottdesign.com>
2021-10-18 15:40:48 -0700

    Add /presigned_url route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :sessions, only: [:create, :index]
-      resources :movie_details, only: [:create, :index], path: '/movie-details'
+      resources :movie_details, only: [:create, :index, :update], path: '/movie-details'
+      post '/presigned_url', to: 'direct_upload#create'
     end
   end
 end
