9741ae8a6ed6b8ed8cee5159d8d4c064c57fa5be
Michael Abbott <michael@mjabbottdesign.com>
2021-10-18 15:29:53 -0700

    Create upload movie poster test

--- spec/requests/movie_details_spec.rb
+++ spec/requests/movie_details_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'sessions API' do
+RSpec.describe 'movie details API' do
   it 'returns the user credentials after a succesfull login', :vcr do
    user =  User.create!(
       email: "whatever@example.com",
@@ -10,7 +10,7 @@ RSpec.describe 'sessions API' do
       password_confirmation: "password"
     )
 
-    body = { 
+    body = {
       title: 'Gangsta Squirrel Dragons Attack the Scrumdillyuptious Hippopotamus',
       user_id: user.id,
       genre: "horror",
@@ -61,7 +61,65 @@ RSpec.describe 'sessions API' do
     expect(movie_detail[:data][:attributes][:production_company]).to be_a(String)
     expect(movie_detail[:data][:attributes]).to have_key(:distribution)
     expect(movie_detail[:data][:attributes][:distribution]).to be_a(String)
-  end 
-end 
+  end
 
+  describe "upload movie poster" do
+    xit "updates the film_epk record with a movie_poster_url", :vcr do
+      user =  User.create!(
+         email: "whatever@example.com",
+         first_name: "First",
+         last_name: "Last",
+         password: "password",
+         password_confirmation: "password"
+       )
 
+      epk = FilmEpk.create!(
+        user_id: user.id,
+        movie_title: "The Best"
+      )
+
+      body1 = {
+        file: {
+               filename: "test_upload",
+               byte_size: 92358,
+               checksum: "UCo4+JMJDVuxmSASPcz+Wg==",
+               content_type: "image/jpeg",
+               metadata: {
+               message: "active_storage_test"
+              }
+        }
+      }
+
+      post '/api/v1/presigned_url', params: body1, as: :json
+      response_body1 = JSON.parse(response.body, symbolize_names: true)
+
+      body2 = File.binread('spec/assets/amazing_grace_movie_poster.jpg')
+      headers2 = {
+        "Content-MD5": response_body1[:direct_upload][:headers][:"Content-MD5"],
+        "Content-Type": response_body1[:direct_upload][:headers][:"Content-Type"]
+      }
+# direct_upload url changes each time the post '/api/v1/presigned_url' is called - need to be static. stub?
+      Faraday.put(response_body1[:direct_upload][:url]) do |f|
+        f.body = body2
+        f.headers = headers2
+      end
+
+      body3 = {
+                movie_poster: "#{response_body1[:blob_signed_id]}"
+              }
+
+      patch "/api/v1/movie-details/#{epk.id}", params: body3, as: :json
+      response_body3 = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      expect(response_body3).to have_key(:id)
+      expect(response_body3[:id]).to eq(epk.id)
+      expect(response_body3).to have_key(:user_id)
+      expect(response_body3[:user_id]).to eq(user.id)
+      expect(response_body3).to have_key(:movie_poster_url)
+      expect(response_body3[:movie_poster_url]).to be_a(String)
+    end
+  end
+end
