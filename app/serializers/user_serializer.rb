739ea77150d941ade7e3c6a2a6f33e572597a5d1
Michael Abbott <michael@mjabbottdesign.com>
2021-10-19 16:11:33 -0700

    Update UserSerializer to add film_epks attribute

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,7 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :email, :first_name, :last_name
-
+  attributes :email,
+             :first_name,
+             :last_name,
+             :film_epks
 end
