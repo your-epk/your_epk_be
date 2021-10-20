088f5dfb3e0699fd3c271d80669e01561da6e8b3
Michael Abbott <michael@mjabbottdesign.com>
2021-10-20 15:06:08 -0700

    Add user request get test

--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe 'users API' do
     )
   end
 
-  it 'returns the users attributes and epks', :vcr do
+  it 'returns the users attributes and relationships' do
 
     get "/api/v1/users/#{@user.id}"
 
@@ -26,22 +26,36 @@ RSpec.describe 'users API' do
 
     expect(user_info).to have_key(:data)
     expect(user_info[:data]).to be_a(Hash)
-    expect(user_info[:data].keys.count).to eq(3)
+    expect(user_info[:data].keys.count).to eq(4)
     expect(user_info[:data]).to have_key(:id)
     expect(user_info[:data][:id]).to be_a(String)
     expect(user_info[:data]).to have_key(:type)
     expect(user_info[:data][:type]).to eq("user")
     expect(user_info[:data]).to have_key(:attributes)
     expect(user_info[:data][:attributes]).to be_a(Hash)
-    expect(user_info[:data][:attributes].keys.count).to eq(4)
+    expect(user_info[:data][:attributes].keys.count).to eq(3)
     expect(user_info[:data][:attributes]).to have_key(:email)
     expect(user_info[:data][:attributes][:email]).to be_a(String)
     expect(user_info[:data][:attributes]).to have_key(:first_name)
     expect(user_info[:data][:attributes][:first_name]).to be_a(String)
     expect(user_info[:data][:attributes]).to have_key(:last_name)
     expect(user_info[:data][:attributes][:last_name]).to be_a(String)
-    expect(user_info[:data][:attributes]).to have_key(:film_epks)
-    expect(user_info[:data][:attributes][:film_epks]).to be_an(Array)
 
+    expect(user_info[:data]).to have_key(:relationships)
+    expect(user_info[:data][:relationships]).to be_a(Hash)
+    expect(user_info[:data][:relationships]).to have_key(:film_epks)
+    expect(user_info[:data][:relationships][:film_epks]).to be_a(Hash)
+    expect(user_info[:data][:relationships][:film_epks]).to have_key(:data)
+    expect(user_info[:data][:relationships][:film_epks][:data]).to be_an(Array)
+
+    expect(user_info).to have_key(:included)
+    expect(user_info[:included]).to be_an(Array)
+    expect(user_info[:included][0]).to have_key(:id)
+    expect(user_info[:included][0][:id]).to be_a(String)
+    expect(user_info[:included][0]).to have_key(:type)
+    expect(user_info[:included][0][:type]).to eq("film_epk")
+    expect(user_info[:included][0]).to have_key(:attributes)
+    expect(user_info[:included][0][:attributes]).to be_a(Hash)
+    expect(user_info[:included][0][:attributes].keys.count).to eq(12)
   end
 end
