fc469a0bf289ba1d0234fd2ec8cef0123bfd0419
Michael Abbott <michael@mjabbottdesign.com>
2021-10-20 15:08:01 -0700

    Add include :film_epks to UserSerializer instance in #show

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::UsersController < ApplicationController
   def show
     user = User.find_by(id: user_params[:id])
-    render json: UserSerializer.new(user)
+    render json: UserSerializer.new(user, include: [:film_epks])
   end
 
   private
