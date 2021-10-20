f4e7a9e7d5c790e72b259223584559b9e13baba4
Michael Abbott <michael@mjabbottdesign.com>
2021-10-20 15:07:18 -0700

    Update UserSerializer with has_many :film_epks

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,11 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
+  has_many :film_epks
   attributes :email,
              :first_name,
-             :last_name,
-             :film_epks
+             :last_name
+
+  # attribute :film_epks do |object|
+  #   UserFilmEpkSerializer.new(object.film_epks)
+  # end
 end
