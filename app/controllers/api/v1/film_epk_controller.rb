76f2493f03146373a955b6e7d06bb9635ce72df1
Elliot Olbright <elliotolbright@gmail.com>
2021-10-20 16:24:44 -0600

    add PressController call

--- app/controllers/api/v1/film_epk_controller.rb
+++ app/controllers/api/v1/film_epk_controller.rb
@@ -13,16 +13,15 @@ class Api::V1::FilmEpkController < ApplicationController
   end
 
   def update
-   movie_poster = film_epk_params[:movie_poster]
-   film_epk = FilmEpk.find_by(id: params[:id].to_i)
+    movie_poster = film_epk_params[:movie_poster]
+    film_epk = FilmEpk.find_by(id: params[:id].to_i)
     if !movie_poster.nil?
       film_epk.movie_poster.attach(movie_poster)
       render json: film_epk.as_json(root: false, methods: :movie_poster_url).except('updated_at')
     end 
-
     render json: AwardsController.new.check(film_epk_params[:award], film_epk) if film_epk_params[:award]
     render json: FilmFamsController.new.check(film_epk_params[:film_fam], film_epk) if film_epk_params[:film_fam]
-
+    render json: PressesController.new.check(film_epk_params[:presses], film_epk) if film_epk_params[:presses]
   end
 
   private
@@ -54,7 +53,8 @@ class Api::V1::FilmEpkController < ApplicationController
              :company_name,
              :header_image,
              award: [:name, :year, :award_type],
-             film_fam: [:role, :first_name, :last_name, :description]
+             film_fam: [:role, :first_name, :last_name, :description], 
+             presses: [:name_of_publication, :description, :link]
            )
   end
 end
