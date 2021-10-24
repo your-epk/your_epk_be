c74026f7a2303247ca746ba908c1016235b698c4
Michael Abbott <michael@mjabbottdesign.com>
2021-10-23 17:23:33 -0700

    Add MoviePostersController #create and #poster_params

---
+++ app/controllers/api/v1/movie_posters_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::MoviePostersController < ApplicationController
+  def create
+    movie_poster = poster_params[:movie_poster]
+    film_epk = FilmEpk.find_by(id: poster_params[:film_epk_id].to_i)
+    if !film_epk.nil?
+      film_epk.movie_poster.attach(movie_poster)
+      # render json: film_epk.as_json(root: false, methods: :movie_poster_url).except('updated_at')
+      render json: { movie_poster_url: film_epk.movie_poster_url.as_json }
+    else
+      render json: { error: "An existing Film Epk id is required" }
+    end
+  end
+
+  private
+  def poster_params
+    params.permit(:movie_poster, :film_epk_id)
+  end
+end
