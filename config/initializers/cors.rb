8a4ff249def256806a5e56e0245141ed6a8a38c6
Michael Abbott <michael@mjabbottdesign.com>
2021-10-23 19:35:34 -0700

    Update cors with FE heroku url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'http://localhost:3000', 'http://your-epk.herokuapp.com'
     resource '*', headers: :any, methods: [:get, :post, :patch, :delete]
   end
 end
