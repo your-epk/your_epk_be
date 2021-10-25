41dfd8d82bfdce9df62faa2414411f22cd9ff819
Elliot Olbright <elliotolbright@gmail.com>
2021-10-25 15:55:19 -0600

    ignore welcome page

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,7 @@ SimpleCov.start 'rails' do
   add_filter "app/channels/application_cable"
   add_filter "app/jobs"
   add_filter "app/mailers"
+  add_filter "app/controllers/welcome_controller.rb"
 
 end
 
