df3073d0a6bc38442b93b2a73182125e0eb16e2d
Elliot Olbright <elliotolbright@gmail.com>
2021-10-25 17:46:15 -0600

    create headshots controller

---
+++ app/controllers/api/v1/head_shots_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::HeadShotsController < ApplicationController
+  def create
+    head_shot = hs_params[:blob_signed_id]
+    film_fam = FilmFam.find_by(id: hs_params[:film_fam_id].to_i)
+    if !film_fam.nil?
+      film_fam.head_shot.attach(head_shot)
+
+      render json: { head_shot_url: film_fam.head_shot_url.as_json }
+    else
+      render json: { error: "An existing Film Fam id is required" }
+    end
+  end
+
+  private
+  def hs_params
+    params.permit(:blob_signed_id, :film_fam_id)
+  end
+end 
