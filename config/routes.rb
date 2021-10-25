86d50d9e8f738016c9b3a87528e09d962dac2688
Elliot Olbright <elliotolbright@gmail.com>
2021-10-25 16:17:46 -0600

    delete uneeded route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :sessions, only: [:create, :index]
+      resources :sessions, only: [:create]
       resources :film_epk, only: [:create, :index, :update, :destroy, :show]
       resources :users, only: [:show]
       resources :awards, only: [:create, :destroy]
