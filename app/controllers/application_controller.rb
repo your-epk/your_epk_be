40507539a6c7996352e3c8bd9c04cb3121c7149b
Elliot Olbright <elliotolbright@gmail.com>
2021-11-08 17:43:30 -0700

    add forgery protection, create CSRF Cookie method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::API
+  include ActionController::Cookies
+  include ActionController::RequestForgeryProtection
+
+
+  protect_from_forgery with: :exception
+
+  before_action :set_csrf_cookie
+
+  helper_method :current_user
+
+  def current_user
+    User.find(session[:user_id]) if session[:user_id]
+  end
+
+
+  def set_csrf_cookie
+    cookies["CSRF-TOKEN"] = form_authenticity_token
+  end
+
 end
