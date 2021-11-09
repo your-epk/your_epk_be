4508b1ca88ef9e8359130d98d54903275260c0cf
Elliot Olbright <elliotolbright@gmail.com>
2021-11-08 17:42:58 -0700

    add user_id to session hash

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,8 +1,12 @@
 class Api::V1::SessionsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create]
+
+  def index
+  end 
 
   def create
     user = User.find_by(email: session_params[:email].downcase)
-
+    session[:user_id] = user.id
     if user.authenticate(session_params[:password])
       render json: SessionSerializer.new(user), status: :ok
     else
