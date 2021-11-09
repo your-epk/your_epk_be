c8a96ea72a40e214cd6afb28f0d7af746447877d
Elliot Olbright <elliotolbright@gmail.com>
2021-11-08 17:42:12 -0700

    Update Cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,10 +14,14 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+Rails.application.config.action_controller.forgery_protection_origin_check = false
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:3000', 'https://your-epk.herokuapp.com'
-    resource '*', headers: :any, methods: [:get, :post, :patch, :delete]
+    resource '*', 
+      headers: :any, 
+      methods: [:get, :post, :put, :patch, :delete, :options, :head], 
+      credentials: true
   end
 end
